01c91caf2c648c5834b4808fb2c1f4153d9f0fbc
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-03-30 21:36:35 +0500

    Initial commit

---
+++ src/main/java/ru/netology/radio/ru/netology/radio.java
@@ -0,0 +1,17 @@
+package ru.netology.radio.ru.netology;
+
+public class radio {
+    int [] numberOfStations;
+    int volume;
+
+    public void choiceOfStation(int numberOfStations) {
+    }
+
+
+    public void choiceOfVolume(int volume){
+
+    }
+    }
+
+
+
---
+++ src/test/java/ru/netology/radio/ru/netology/radioTest.java
@@ -0,0 +1,19 @@
+package ru.netology.radio.ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class radioTest {
+    @Test
+    public void choiceOfStation() {
+    radio myRadio = new radio();
+     int [] numberOfStations = {0, 1, 2, 3, 4, 5, 6, 7, 8,9};
+
+    }
+    public void choiceOfVolume() {
+    int [] volume = {0, 1, 2, 3, 4, 5, 6, 7, 8,9};
+
+
+    }
+
+    }
