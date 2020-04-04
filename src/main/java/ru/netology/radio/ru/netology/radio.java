79936072ffadc9aa055b7e2f3041c3a33de42819
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-04-04 23:33:24 +0500

    Add void

--- src/main/java/ru/netology/radio/ru/netology/radio.java
+++ src/main/java/ru/netology/radio/ru/netology/radio.java
@@ -1,31 +1,35 @@
 package ru.netology.radio.ru.netology;
 
 public class radio {
-    private final static int MAX_STATION = 10;
+    private final static int MAX_STATION = 9;
     private final static int MIN_STATION = 0;
-    private final static int MAX_VOLUME = 10;
+    private final static int MAX_VOLUME = 9;
     private final static int MIN_VOLUME = 0;
     private int currentStation;
     private int volume;
 
-    public setStation(int Station) {
+    public void setStation(int Station) {
         currentStation = 0;
     }
 
     public void next () {
         if (currentStation<MAX_STATION)
             currentStation=+1;
+        if (currentStation==MAX_STATION)
+            currentStation=0;
     }
     public void prev() {
         if (currentStation>MIN_STATION)
             currentStation=-1;
+        if (currentStation==MIN_STATION)
+            currentStation=9;
     }
 
-    public increaseVolume (){
+    public void increaseVolume (){
         if (volume<MAX_VOLUME)
             volume=+1;
     }
-    public decreaseVolume (){
+    public void decreaseVolume (){
         if (volume>MIN_VOLUME)
             volume=+1;
     }
