7c0099e90af0d738d6ec847d6cca290815b19251
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-04-04 23:23:36 +0500

    Delete radioTest))

--- src/main/java/ru/netology/radio/ru/netology/radio.java
+++ src/main/java/ru/netology/radio/ru/netology/radio.java
@@ -1,17 +1,36 @@
 package ru.netology.radio.ru.netology;
 
 public class radio {
-    int [] numberOfStations;
-    int volume;
+    private final static int MAX_STATION = 10;
+    private final static int MIN_STATION = 0;
+    private final static int MAX_VOLUME = 10;
+    private final static int MIN_VOLUME = 0;
+    private int currentStation;
+    private int volume;
 
-    public void choiceOfStation(int numberOfStations) {
+    public setStation(int Station) {
+        currentStation = 0;
     }
 
-
-    public void choiceOfVolume(int volume){
-
+    public void next () {
+        if (currentStation<MAX_STATION)
+            currentStation=+1;
     }
+    public void prev() {
+        if (currentStation>MIN_STATION)
+            currentStation=-1;
     }
+
+    public increaseVolume (){
+        if (volume<MAX_VOLUME)
+            volume=+1;
+    }
+    public decreaseVolume (){
+        if (volume>MIN_VOLUME)
+            volume=+1;
+    }
+
+}
 
 
 
