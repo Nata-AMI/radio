df10eb1662b5f64f88cccf2ae65836814a3c4191
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-04-05 23:59:21 +0500

    Final Change

--- src/main/java/ru/netology/radio/ru/netology/Radio.java
+++ src/main/java/ru/netology/radio/ru/netology/Radio.java
@@ -2,40 +2,51 @@ package ru.netology.radio.ru.netology;
 
 public class Radio {
     private final static int MAX_STATION = 9;
-    private final static int MIN_STATION = 0;
+    private final static int MIN_STATION = 1;
     private final static int MAX_VOLUME = 10;
     private final static int MIN_VOLUME = 0;
-    private int currentStation;
-    private int volume;
+    public int currentStation;
+    public int volume;
 
+    public void next () {
+        if (currentStation < MAX_STATION)
+            this.currentStation =currentStation +1;
+        if (currentStation == MAX_STATION)
+            this.currentStation = 1;
+       }
+
+    public void prev () {
+        if (currentStation>MIN_STATION)
+            this.currentStation=currentStation-1;
+        if (currentStation==MIN_STATION)
+            this.currentStation=9;
+
+    }
+
+    public void increaseVolume (){
+        if (volume<MAX_VOLUME)
+            this.volume=volume+1;
+        if (volume==MAX_VOLUME)
+            this.volume=volume;
+    }
+    public void decreaseVolume (){
+        if (volume>MIN_VOLUME)
+            this.volume=volume-1;
+        if (volume==MIN_VOLUME)
+            this.volume=volume;
+    }
     public void setStation(int currentStation) {
-        currentStation = 0;
+        this.currentStation = currentStation;
     }
     public int getCurrentStation(){
         return currentStation;
     }
 
-    public void next () {
-        if (currentStation < MAX_STATION)
-            currentStation = +1;
-        if (currentStation == MAX_STATION)
-            currentStation = 0;
-       }
-
-    public void prev() {
-        if (currentStation>MIN_STATION)
-            currentStation=-1;
-        if (currentStation==MIN_STATION)
-            currentStation=9;
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
-
-    public void increaseVolume (){
-        if (volume<MAX_VOLUME)
-            volume=+1;
-    }
-    public void decreaseVolume (){
-        if (volume>MIN_VOLUME)
-            volume=+1;
+    public int getVolume(){
+        return volume;
     }
 
 }
--- src/test/java/ru/netology/radio/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/radio/ru/netology/RadioTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class radioTest {
+class RadioTest {
+
 
     @Test
     void next() {
@@ -12,6 +13,30 @@ class radioTest {
         radio.setStation(7);
         radio.next();
         assertEquals(8, radio.getCurrentStation());
+    }
+
+    @Test
+    void prev() {
+        Radio radio = new Radio();
+        radio.setStation(7);
+        radio.prev();
+        assertEquals(6, radio.getCurrentStation());
+    }
+
+    @Test
+    void decreaseVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(7);
+        radio.decreaseVolume();
+        assertEquals(6, radio.getVolume());
+    }
+
+    @Test
+    void increaseVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(7);
+        radio.increaseVolume();
+        assertEquals(8, radio.getVolume());
     }
 }
 
